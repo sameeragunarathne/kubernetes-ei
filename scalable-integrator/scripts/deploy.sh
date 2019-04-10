a98a0d056d1d21bd95f85b4ca3a81cfc4c0dbb0b
Kamidu Punchihewa <kamidu@Kamidus-MacBook-Pro.local>
2019-04-10 22:29:42 +0530

    verifone related config changes

--- scalable-integrator/scripts/deploy.sh
+++ scalable-integrator/scripts/deploy.sh
@@ -25,41 +25,41 @@ function echoBold () {
     ${ECHO} -e $'\e[1m'"${1}"$'\e[0m'
 }
 
-function usage () {
-    echoBold "This script automates the installation of Kubernetes resources for WSO2 Enterprise Integrator's Integrator profile\n"
-    echoBold "Allowed arguments:\n"
-    echoBold "-h | --help"
-    echoBold "--wu | --wso2-username\t\tYour WSO2 username"
-    echoBold "--wp | --wso2-password\t\tYour WSO2 password\n\n"
-}
+#function usage () {
+#    echoBold "This script automates the installation of Kubernetes resources for WSO2 Enterprise Integrator's Integrator profile\n"
+#    echoBold "Allowed arguments:\n"
+#    echoBold "-h | --help"
+#    echoBold "--wu | --wso2-username\t\tYour WSO2 username"
+#    echoBold "--wp | --wso2-password\t\tYour WSO2 password\n\n"
+#}
 
-WSO2_SUBSCRIPTION_USERNAME=''
-WSO2_SUBSCRIPTION_PASSWORD=''
+#WSO2_SUBSCRIPTION_USERNAME=''
+#WSO2_SUBSCRIPTION_PASSWORD=''
 
 # capture named arguments
-while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
-
-    case ${PARAM} in
-        -h | --help)
-            usage
-            exit 1
-            ;;
-        --wu | --wso2-username)
-            WSO2_SUBSCRIPTION_USERNAME=${VALUE}
-            ;;
-        --wp | --wso2-password)
-            WSO2_SUBSCRIPTION_PASSWORD=${VALUE}
-            ;;
-        *)
-            echoBold "ERROR: unknown parameter \"${PARAM}\""
-            usage
-            exit 1
-            ;;
-    esac
-    shift
-done
+#while [ "$1" != "" ]; do
+#    PARAM=`echo $1 | awk -F= '{print $1}'`
+#    VALUE=`echo $1 | awk -F= '{print $2}'`
+#
+#    case ${PARAM} in
+#        -h | --help)
+#            usage
+#            exit 1
+#            ;;
+#        --wu | --wso2-username)
+#            WSO2_SUBSCRIPTION_USERNAME=${VALUE}
+#            ;;
+#        --wp | --wso2-password)
+#            WSO2_SUBSCRIPTION_PASSWORD=${VALUE}
+#            ;;
+#        *)
+#            echoBold "ERROR: unknown parameter \"${PARAM}\""
+#            usage
+#            exit 1
+#            ;;
+#    esac
+#    shift
+#done
 
 # create a new Kubernetes Namespace
 ${KUBECTL} create namespace wso2
@@ -71,7 +71,7 @@ ${KUBECTL} create serviceaccount wso2svc-account -n wso2
 ${KUBECTL} config set-context $(${KUBECTL} config current-context) --namespace=wso2
 
 # create a Kubernetes Secret for passing WSO2 Private Docker Registry credentials
-${KUBECTL} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
+#${KUBECTL} create secret docker-registry wso2creds --docker-server=docker.wso2.com --docker-username=${WSO2_SUBSCRIPTION_USERNAME} --docker-password=${WSO2_SUBSCRIPTION_PASSWORD} --docker-email=${WSO2_SUBSCRIPTION_USERNAME}
 
 # create Kubernetes Role and Role Binding necessary for the Kubernetes API requests made from Kubernetes membership scheme
 ${KUBECTL} create -f ../../rbac/rbac.yaml
